67cf93f50fcb87bda2e893b58f3754490e231ec8
m <m@bitsnbites.eu>
2017-09-03 12:30:37 +0200

    UI: Change text and arrow to Resseract gold

--- src/engine/rendertext.cpp
+++ src/engine/rendertext.cpp
@@ -268,7 +268,7 @@ static void text_color(char c, char *stack, int size, int &sp, bvec color, int a
             case '5': color = bvec(192,  64, 192); break;   // magenta
             case '6': color = bvec(255, 128,   0); break;   // orange
             case '7': color = bvec(255, 255, 255); break;   // white
-            case '8': color = bvec( 80, 207, 229); break;   // "Resseract Blue"
+            case '8': color = bvec(224, 190, 100); break;   // "Resseract Gold" (0xE0BE64)
             case '9': color = bvec(160, 240, 120); break;
             default: gle::color(color, a); return;          // provided color: everything else
         }
